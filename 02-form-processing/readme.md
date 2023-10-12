63e793ad4a7e70468cc1d50a7a4d34c8605707c4
kunxin-chor <chorkunxin@yahoo.com>
2023-10-12 13:29:33 +0000

    Added query checked checkboxes and select

--- 02-form-processing/readme.md
+++ 02-form-processing/readme.md
@@ -1,2 +1,6 @@
 # Steps to create an interactive JavaScript web app
 1. What `events` are we listening to? For the form example, the event when the user clicks on the button (aka event driven programming)
+
+2. What `variables` do I need?
+
+3. What is the `input` do I need to read from
--- 02-form-processing/script.js
+++ 02-form-processing/script.js
@@ -1,17 +1,45 @@
 const button = document.querySelector("#submit-btn");
+
+
 button.addEventListener("click", function(){
     // do whatever we want to do when the click is detected
  
     let firstName;
-    let firstNameElement = document.querySelector("#first-name");
+    let firstNameElement = document.querySelector("#first-name")
+
     firstName = firstNameElement.value;
     console.log("firstName =", firstName);
 
     let lastName = document.querySelector("#last-name").value;
 
+    // we are only interested in one radio button (the one that is checked)
+    // hence we are using querySelector 
+    let checkedRadioButton = document.querySelector(".fish:checked");
+    // let eatRawFish = null;
+    // if (checkedRadioButton) {
+    //     eatRawFish = checkedRadioButton.value;
+    // }
+
+    // alternative: use optional chaining
+    // if checkedRadioButton is null, stop evaluating the expression and 
+    // evaluate to null
+    // otherwise, get value property of checkedRadioButton
+    let eatRawFish = checkedRadioButton?.value;
+    console.log(eatRawFish);
+
+    let selectedCheckboxes = document.querySelectorAll(".country:checked");
+    console.log(selectedCheckboxes);
+    let countries = [];
+    for (let c of selectedCheckboxes) {
+        countries.push(c.value);
+    }
+    console.log("countries =", countries);
+
     console.log("first name =", firstName);
     console.log("last name =", lastName);
 
-
+    let occupationDropdown= document.querySelector("#occupation");
+    let occupation = occupationDropdown.value;
+    console.log("occupation = ", occupation);
 
 })
