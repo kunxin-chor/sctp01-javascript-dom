98b68b9ff8c1908587c6cf1a5186b1bf2cb04493
kunxin-chor <chorkunxin@yahoo.com>
2023-11-01 12:49:37 +0000

    Added foursquare API search

--- 14-foursquare-api/data.js
+++ 14-foursquare-api/data.js
@@ -2,6 +2,7 @@
 async function find(searchTerms, lat, lng, radius=1000) {
 
     const response = await axios.get("https://api.foursquare.com/v3/places/search",{
+       // check the FourSquare documentation (the Places API)
         params: {
             query: searchTerms, 
             ll: lat+","+lng, 
@@ -10,6 +11,9 @@ async function find(searchTerms, lat, lng, radius=1000) {
         },
         headers: {
             accept: 'application/json',
+            // Please use your own API Key
+            // By right: the API key shouldn't be here however right now
+            // there is no way to secure the key
             Authorization: 'fsq3jhVKTC5HY3RD1RoOpjxNLUSVMxf122XO4sUGIqpAA2w='
             }
     });
